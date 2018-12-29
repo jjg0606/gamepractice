19e78dd97ee984e20e46a9df5a0fcf3911510540
jjg0606 <ccm7772003@naver.com>
2018-12-29 23:05:43 +0900

    plz

---
+++ Assets/Assets/4. Scripts/PanelNextSlot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelNextSlot : MonoBehaviour {
+
+    public TetSlot[] slotList = new TetSlot[16];
+    public TetBlock tetBlock = null;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateTetBlock(TetBlock block)
+    {
+        TetBlock tempBlock = new TetBlock(block.shape);
+        tempBlock.RotateCounterClockwise();
+        if(tetBlock!=null)
+        {
+            foreach(var pos in tetBlock.position)
+            {
+                slotList[(int)(5 + pos.x + 4 * (pos.y))].UpdateToBlack();                
+            }
+        }
+        tetBlock = tempBlock;
+
+        foreach (var pos in tetBlock.position)
+        {
+            slotList[(int)(5 + pos.x + 4 * (pos.y))].UpdateColor(block.color);
+        }
+
+    }
+}
---
+++ Assets/Assets/4. Scripts/TetSlot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class TetSlot : MonoBehaviour {
+
+    public bool isFilled=false;
+        
+    public void UpdateColor(Color color)
+    {
+        GetComponent<RawImage>().color = color;
+    }
+
+    public void UpdateToBlack()
+    {
+        GetComponent<RawImage>().color = Color.black;
+        isFilled = false;
+    }
+
+   
+
+    public void UpdateToFilled()
+    {
+        isFilled = true;
+    }
+
+   
+
+
+
+}
